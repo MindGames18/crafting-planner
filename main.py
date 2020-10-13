c8a399518de8877cb0ae27540c4395fa188d8005
jnishanthbabu <50679606+jnishanthbabu@users.noreply.github.com>
2020-10-13 16:12:42 +0530

    snapshot working GUI

--- main.py
+++ main.py
@@ -1,42 +1,67 @@
 import asset_manager as am
 import PySimpleGUI as sg
+import json_read as jr
 
 
-sg.theme('BluePurple')
+def loader_gui():
+    sg.theme('BluePurple')
 
-version_list = ['1.12.2', 'WIP - NO USES']
+    version_list = ['1.12.2', 'WIP - NO USES']
 
-# The GUI Layout
-layout = [[sg.Text('Choose Your Minecraft Version'), sg.Text(size=(10, 1), key='-OUTPUT-')],
-          [sg.Image(filename=None)],
-          [sg.OptionMenu((version_list), default_value=None,
-                         key='selected_version', tooltip='Version')],
-          [sg.Button('Load'), sg.Button('Exit'), sg.Button('Delete Local Cache')]]
+    # The GUI Layout
+    layout = [[sg.Text('Choose Your Minecraft Version'), sg.Text(size=(10, 1), key='-OUTPUT-')],
+              [sg.Image(filename=None)],
+              [sg.OptionMenu((version_list), default_value=None,
+                             key='selected_version', tooltip='Version')],
+              [sg.Button('Load'), sg.Button('Exit'), sg.Button('Delete Local Cache')]]
 
-window = sg.Window('Minecraft Loader', layout, size=(250, 100))
+    window = sg.Window('Minecraft Loader', layout, size=(250, 100))
+    while True:  # Event Loop
+        event, values = window.read()
+        if event == sg.WIN_CLOSED or event == 'Exit':
+            window.close()
+            break
+        # Event To load the Assets File into the cache
+        if event == 'Load':
+            version = values['selected_version']
+            try:
+                am.extracting_assets(version)
+                sg.popup('Assets Extracted')
+            except:
+                sg.popup("Assets Could Not be extracted")
+            # Event To Delete the Cache files
 
-while True:  # Event Loop
-    event, values = window.read()
-    print(event, values)
-    if event == sg.WIN_CLOSED or event == 'Exit':
-        break
+        if event == "Delete Local Cache":
+            try:
+                am.delete_assets()
+                sg.popup("Local Cache Deleted")
+            except:
+                sg.popup("Could not delete local cache")
 
-    # Event To load the Assets File into the cache
-    if event == 'Load':
-        version = values['selected_version']
-        try:
-            am.extracting_assets(version)
-            sg.popup('Assets Extracted')
-            sg.popup("test pop")
-        except:
-            sg.popup("Assets Could Not be extracted")
 
-    # Event To Delete the Cache files
-    if event == "Delete Local Cache":
-        try:
-            am.delete_assets()
-            sg.popup("Local Cache Deleted")
-        except:
-            sg.popup("Could not delete local cache")
+def main_ui():
+    sg.theme('BluePurple')
+    available_recipes = jr.recipe_loader()
+    available_recipes.sort()
+    layout = [
+        [sg.Text('Item Browser')],
+        [sg.Combo(available_recipes, readonly=True, key='recipe_item')],
+        [sg.OptionMenu((['1', '2']), default_value=None,
+                       key='selected_version', tooltip='Version')],
+        [sg.Button(button_text='Asset Loader'), sg.Button('Exit')],
+        [sg.Button(button_text='1', size=(15, 6),border_width=5,image_filename='apple.png', image_size=(64, 64)),sg.Button(button_text='1', size=(15, 6),border_width=5),sg.Button(button_text='1', size=(15, 6),border_width=5)],
+        [sg.Button(button_text='1', size=(15, 6),border_width=5),sg.Button(button_text='1', size=(15, 6),border_width=5),sg.Button(button_text='1', size=(15, 6),border_width=5)],
+        [sg.Button(button_text='1', size=(15, 6),border_width=5),sg.Button(button_text='1', size=(15, 6),border_width=5),sg.Button(button_text='1', size=(15, 6),border_width=5)]
+    ]
+    window = sg.Window('Minecraft Recipe Viewer', layout, size=(800, 600))
+    while True:  # Event Loop
+        event, values = window.read()
+        if event == sg.WIN_CLOSED or event == 'Exit':
+            window.close()
+            break
 
-window.close()
+        if event == 'Asset Loader':
+            loader_gui()
+
+
+main_ui()
