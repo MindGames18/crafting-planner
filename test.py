58ed5e2ea988ed0127d98e2ffcdc7a61c60e0984
CookieCoder <adityasinha95@gmail.com>
2020-10-15 03:34:25 +0530

    trying to add dict

--- test.py
+++ test.py
@@ -4,55 +4,67 @@ import json
 
 
 #recipe_name = 
+total =0
 ing_name = []
 recipe_count = 0
 newpat = [[0,0,0],[0,0,0],[0,0,0]]
-filename = "yellow_wool.json"
+#filename = "yellow_wool.json"
 count = 0
 
 
-#for filename in os.listdir(c.abs_recipe_dir):
-abs_file_path = c.abs_recipe_dir + filename
-read_data = open(abs_file_path).read()  # File Handle
-data = json.loads(read_data)  # Json object
+for filename in os.listdir(c.abs_recipe_dir):
+    abs_file_path = c.abs_recipe_dir + filename
+    read_data = open(abs_file_path).read()  # File Handle
+    data = json.loads(read_data)  # Json object
 
-name = data["result"]["item"]
-name = name.replace("minecraft:", "")
-ing_name = []
+    name = data["result"]["item"]
+    name = name.replace("minecraft:", "")
+    book = {}
 
-if data["type"] == "crafting_shaped":
-    for i in range(len(data["pattern"])):
-        for j in range(len(data["pattern"][0])):
-            newpat[i][j] = data["pattern"][i][j]
+    if data["type"] == "crafting_shaped":
+        for i in range(len(data["pattern"])):
+            for j in range(len(data["pattern"][0])):
+                newpat[i][j] = data["pattern"][i][j]
 
     
-    for i in range(len(newpat)):
-        for j in range(len(newpat)):
-            if newpat[i][j] in data["key"]:
-                if type(data["key"][newpat[i][j]]) is dict:
-                    ing_name.append(data["key"][newpat[i][j]]["item"])
-                    print(ing_name[count])
-                    count+=1
+        for i in range(len(newpat)):
+            for j in range(len(newpat)):
+                if newpat[i][j] in data["key"]:
+                    if type(data["key"][newpat[i][j]]) is dict:
+                        ing_name.append(data["key"][newpat[i][j]]["item"])
+                        print(ing_name[count])
+                        count+=1
             
+                    else:
+                        ing_name.append(data["key"][newpat[i][j]][0]["item"])
+                        print(ing_name[count])
+                        count+=1
                 else:
-                    ing_name.append(data["key"][newpat[i][j]][0]["item"])
+                    ing_name.append("NULL")
                     print(ing_name[count])
                     count+=1
-            else:
-                ing_name.append("NULL")
-                print(ing_name[count])
-                count+=1
    
 
-    print(name,"\n",ing_name)
+        #print(name,"\n",ing_name)
+
+    elif data["type"] == "crafting_shapeless":
+        for item in data["ingredients"]:
+            print(data["ingredients"][count]["item"])
+            ing_name.append(data["ingredients"][count]["item"])
+            count+=1
+        for i in range(9-count):
+            ing_name.append("NULL")
+        #print(ing_name)
+
+    book[name] = ing_name
     ing_name = []
     newpat = [[0,0,0],[0,0,0],[0,0,0]]
+    count = 0
+    total+=1
+    if total == 2:
+        break
+    else:
+        continue
 
-elif data["type"] == "crafting_shapeless":
-    for item in data["ingredients"]:
-        print(data["ingredients"][count]["item"])
-        ing_name.append(data["ingredients"][count]["item"])
-        count+=1
-    for i in range(9-count):
-        ing_name.append("NULL")
-    print(ing_name)
+
+print(book[0])
