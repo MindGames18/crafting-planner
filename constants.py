36aec25702e2c4cbd12a787cd47ebc0d7c256a9f
jnishanthbabu <50679606+jnishanthbabu@users.noreply.github.com>
2020-10-17 05:44:28 +0530

    added block texture dir to constants

--- constants.py
+++ constants.py
@@ -9,6 +9,10 @@ import os
 script_dir = os.path.dirname(__file__)
 jar_dir = "\\jar"
 recipe_dir = "\\assets\\minecraft\\recipes\\"
+
 abs_recipe_dir = script_dir + jar_dir + recipe_dir
 texture_dir = "\\assets\\minecraft\\textures\\items\\"
 abs_texture_dir = script_dir + jar_dir + texture_dir
+
+texture_block_dir = "\\assets\\minecraft\\textures\\blocks\\"
+abs_texture_block_dir = script_dir + jar_dir + texture_block_dir
